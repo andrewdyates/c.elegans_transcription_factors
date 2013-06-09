bee0075edfb8d62c9f4b11563a11b42d2ffe53ab
Andrew Yates <andrewyates.name@gmail.com>
2013-06-09 18:14:53 -0400

    gold genes entrez IDs

---
+++ gold_genes_to_entrezg.py
@@ -0,0 +1,20 @@
+"""pal-1 encodes a homeodomain protein that is the C. elegans Caudal ortholog; PAL-1 is required maternally and embryonically for specification of the C and D posterior blastomere fates and thus, for proper bodywall muscle and hypodermal development during embryogenesis; PAL-1 is also required postembryonically for fate specification of the posterior V6 neuroectoblast during male tail development; in regulating embryonic bodywall muscle specification, PAL-1 binds to an enhancer element in the promoter of hlh-1, the sole C. elegans MRF family member, to positively regulate hlh-1 transcription; PAL-1 is expressed in posterior blastomeres beginning at the four-cell stage of embryogenesis and its expression continues during embryogenesis in posterior-derived tissues and cells undergoing movement or forming midline structures; pal-1 is subject to both transcriptional and translational regulation."""
+
+gold_genes = {
+  "pal-1": "175638",
+  "tbx-8": "176450",
+  "tbx-9": "176449",
+  "elt-1": "177794",
+  "cwn-1": "173399",
+  "hnd-1": "183457",
+  "scrt-1": "183848",
+  "elt-3": "181503",
+  "nhr-25": "181432",
+  "nob-1": "176641",
+  "vab-7": "176521",
+  "unc-120", "172636",
+  "hlh-1", "173788",
+  "mab-21", "175607",
+  "lin-26", "3565051"
+}
+
